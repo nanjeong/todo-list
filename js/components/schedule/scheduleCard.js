5e48d11cd44aaee21c255d0aabdaf4ad92427a46
nanjeong <whdwkxj7@naver.com>
2022-04-08 00:09:52 +0900

    feat: 등록된 카드를 더블 클릭하면 카드 수정 블록으로 변경

--- js/components/schedule/scheduleCard.js
+++ js/components/schedule/scheduleCard.js
@@ -1,4 +1,7 @@
 export class ScheduleCard {
+    LIMIT = 500;
+    clickCount = 0;
+    count = 0;
     constructor(target, cardData) {
         this.$target = target;
         this.cardData = cardData;
@@ -7,11 +10,81 @@ export class ScheduleCard {
 
     init() {
         this.render();
+        this.setEvent();
     }
 
     render() {
         const $scheduleCard = this.template();
         this.$target.insertAdjacentHTML("afterbegin", $scheduleCard);
+    }
+
+    setEvent() {
+        this.$target.addEventListener(
+            "click",
+            this.cardClickEventHandler.bind(this)
+        );
+    }
+
+    cardClickEventHandler({ target }) {
+        this.clickCount += 1;
+
+        if (this.clickCount === 1) {
+            setTimeout(() => {
+                this.clickCount = 0;
+            }, 300);
+            return;
+        } else if (this.clickCount === 2) {
+            this.editScheduleCard.call(this, target);
+            this.clickCount = 0;
+        }
+    }
+
+    editScheduleCard(target) {
+        this.replaceCard2EditCard(target);
+    }
+
+    replaceCard2EditCard(target) {
+        const selectedCard = target.closest(".schedule-card");
+        if (!selectedCard) return;
+
+        const editCard = this.createEditCard(selectedCard);
+        this.$target.replaceChild(editCard, selectedCard);
+    }
+
+    createEditCard(selectedCard) {
+        const cardId = selectedCard.dataset.cardId;
+        const editCard = document.createElement("div");
+
+        editCard.classList.add("schedule-edit-card");
+        editCard.dataset.cardId = cardId;
+        editCard.innerHTML = this.editCardTemplate();
+
+        return editCard;
+    }
+
+    editCardTemplate() {
+        return `<form class="schedule-edit-card__text-container">
+                    <textarea 
+                        class="schedule-edit-card__title"  
+                        placeholder="제목을 입력하세요"
+                        rows="1"
+                        maxLength="${this.LIMIT}"
+                    ></textarea>
+                    <textarea 
+                        class="schedule-edit-card__body" 
+                        placeholder="내용을 입력하세요"
+                        rows="1"
+                        maxLength="${this.LIMIT}"
+                    ></textarea>
+                </form>
+                <div class="schedule-edit-card__btns-container">
+                    <button class="schedule-edit-card__cancel-btn">
+                        취소
+                    </button>
+                    <button class="schedule-edit-card__edit-btn inactive">
+                        수정
+                    </button>
+                </div>`;
     }
 
     template() {
