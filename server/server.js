c9c5e77fd8455671bf8ba6f97cb2e5a74a94842c
nanjeong <whdwkxj7@naver.com>
2022-04-15 01:27:45 +0900

    chore: npm start 명령 변경

--- server/server.js
+++ server/server.js
@@ -3,15 +3,10 @@ const path = require("path");
 
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "/db.json"));
-const middlewares = jsonServer.defaults({
-    static: path.join(__dirname, "/../app/"),
-});
 
 const port = process.env.PORT || 3000;
 
-server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
-    console.log(__dirname);
     console.log("JSON Server is running");
 });
