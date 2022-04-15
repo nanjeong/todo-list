c79eff5c7c7ef1cdd1784e37f4486f1ef092f39b
nanjeong <whdwkxj7@naver.com>
2022-04-15 10:47:31 +0900

    맨 처음 설정처럼 돌아감

--- server/server.js
+++ server/server.js
@@ -3,8 +3,11 @@ const path = require("path");
 
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "/db.json"));
+const middlewares = jsonServer.defaults();
+
 const port = process.env.PORT || 3000;
 
+server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
     console.log("JSON Server is running");
